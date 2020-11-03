e21207c1055c18a45d795edbed57e6a7669c350f
Valentin Böck <valentin.bock@edu-kw1c.nl>
2020-11-03 11:13:19 +0100

    New Repo

---
+++ MaxservVars.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * User: Valentin Böck
+ * Date: 02-10-2020
+ * Time: Time
+ * File: Back-end.php
+ */?>
+
+<?php
+$h1Explore = '<h1>Ontdek al <strong>onze projecten</strong> </h1>';
+
+$exploreProjects = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	 	Aliquam congue urna at ullamcorper cursus. Aliquam semper tincidunt vestibulum.
+	 	Fusce quis nunc nunc. Vivamus porta nec lectus eu finibus.
+	 	Praesent quis sodales ex.
+	 	Etiam vehicula enim at fermentum facilisis.
+	 	Aliquam erat volutpat.
+	 	Sed ut justo est.
+	 	Nullam commodo vitae lacus sed tincidunt.
+	 	Nunc velit urna, iaculis in interdum vitae, fermentum eu nisi.
+	 	Vestibulum bibendum arcu nibh, vel suscipit sem tincidunt eget.
+	 	Suspendisse tristique eleifend velit, vitae porta enim rutrum eu.
+	 	Sed aliquet justo sit amet pellentesque lacinia.
+	 	Suspendisse sapien erat, condimentum a arcu in, vehicula porta dolor.";
+
+echo "$h1Explore";
+echo "$exploreProjects";
+?>
+
+<?php
+$h1MoreInfo = '<h1>Meer informatie nodig</h1>';
+
+$moreInfoText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	 	Aliquam congue urna at ullamcorper cursus. Aliquam semper tincidunt vestibulum.
+		Fusce quis nunc nunc. Vivamus porta nec lectus eu finibus.
+	 	Praesent quis sodales ex.
+	 	Etiam vehicula enim at fermentum facilisis.
+	 	Aliquam erat volutpat.
+	 	Sed ut justo est.
+	 	Nullam commodo vitae lacus sed tincidunt.
+	 	Nunc velit urna, iaculis in interdum vitae, fermentum eu nisi.
+	 	Vestibulum bibendum arcu nibh, vel suscipit sem tincidunt eget.
+	 	Suspendisse tristique eleifend velit, vitae porta enim rutrum eu.
+	 	Sed aliquet justo sit amet pellentesque lacinia.
+	 	Suspendisse sapien erat, condimentum a arcu in, vehicula porta dolor.";
+
+echo "$h1MoreInfo";
+echo "$moreInfoText";
+?>
+
+<?php
+$h1TheWow = '<h1>Het WAUW effect</h1>';
+
+$theWowEffect = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	 	Aliquam congue urna at ullamcorper cursus. Aliquam semper tincidunt vestibulum.
+	 	Fusce quis nunc nunc. Vivamus porta nec lectus eu finibus.
+	 	Praesent quis sodales ex.
+	 	Etiam vehicula enim at fermentum facilisis.
+	 	Aliquam erat volutpat.
+	 	Nullam commodo vitae lacus sed tincidunt.
+	 	Nunc velit urna, iaculis in interdum vitae, fermentum eu nisi.";
+echo "$h1TheWow";
+echo "$theWowEffect";
+?>
+
+<?php
+$h1FastQuality = '<h1>Snelle en kwaliteits volle projecten</h1>';
+
+$fastQualityText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	 	Aliquam congue urna at ullamcorper cursus. Aliquam semper tincidunt vestibulum.
+	 	Fusce quis nunc nunc. Vivamus porta nec lectus eu finibus.
+	 	Praesent quis sodales ex.
+	 	Etiam vehicula enim at fermentum facilisis.
+	 	Aliquam erat volutpat.
+	 	Nullam commodo vitae lacus sed tincidunt.
+	 	Nunc velit urna, iaculis in interdum vitae, fermentum eu nisi.
+	 	Vestibulum bibendum arcu nibh, vel suscipit sem tincidunt eget.";
+echo "$h1FastQuality";
+echo "$fastQualityText";
+?>
+
+<?php
+$h1SearchingFor = '<h1>Op zoek naar ideeën voor ow website</h1>';
+
+$searchingForIdeas = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+	 	Aliquam congue urna at ullamcorper cursus. Aliquam semper tincidunt vestibulum.
+	 	Fusce quis nunc nunc. Vivamus porta nec lectus eu finibus.
+	 	Praesent quis sodales ex.
+	 	Etiam vehicula enim at fermentum facilisis.
+	 	Aliquam erat volutpat.
+	 	Sed ut justo est.";
+echo "$h1SearchingFor";
+echo "$searchingForIdeas";
+?>
+
